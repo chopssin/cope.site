1337b522a671cdf131e2daec41b7dbf9f4215ae9
Chopssin <chopssin@gmail.com>
2017-10-29 16:20:58 +0800

    use cope.useSocketIO instead of cope.config and redesign the whole structure for this method

--- cope.js
+++ cope.js
@@ -2,17 +2,30 @@ var debug = require('debug')('cope.site:cope');
 
 module.exports = function() {
   var cope = {};
+ 
+  cope.useSocketIO = useSocketIO;
+  cope.useMongoDb = useMongoDb;
+  cope.user = user;
+  cope.graph = graph;
 
   // cope.user API
-  cope.user = function() {
-    var user = {},
-        userData = null;
+  function user() {
+    var userAPI = {};
 
-    user.fetch = function() {
+    userAPI.fetch = fetch;
+    userAPI.update = update;
+    userAPI.signIn = signIn;
+    userAPI.singOut = signOut;
+    userAPI.useSocketIO = useSocketIO;
+
+    // Private variables
+    let userData = null;
+
+    function fetch() {
       return userData;
-    }; // end of api.fetch
+    }; // end of `fetch` of object `userAPI`
 
-    user.update = function(params) {
+    function update(params) {
       if (!params || typeof params != 'object') {
         userData = null;
         return;
@@ -21,46 +34,48 @@ module.exports = function() {
       userData.id = params.id;
       userData.email = params.email;
       return;
-    };
+    }; // end of `update` of object `userAPI`
 
-    user.signIn = function(params) {
+    function signIn(params) {
       return new Promise(function(resolve, reject) {
-
-        // Resolve with the current user data; otherwise, 
-        if (userData && userData.id && userData.email) { 
+        // TBD: validate params
+        //if (!params) { 
+        //  promise.reject();
+        //}
+        setTimeout(function() {
+          userData = {};
+          userData.id = 'fakeID';
+          userData.email = params.email;
           resolve(userData);
-
-        // TBD: Sign in with `params`
-        } else {
-          // TBD: validate params
-          //if (!params) { 
-          //  promise.reject();
-          //}
-          setTimeout(function() {
-            userData = {};
-            userData.id = 'fakeID';
-            userData.email = params.email;
-            resolve(userData);
-          }, 300);
-        } // end of else
+        }, 300);
       }); // end of Promise
-    }; // end of user.signIn
+    }; // end of `signIn` of object `userAPI`
 
-    user.signOut = function() {
+    function signOut() {
       return new Promise(function(resolve, reject) {
-        // TBD
+        // TBD: use db
         setTimeout(function() {
           debug('user.signOut', userData.id, userData.email);
           userData = null;
           resolve(userData);
         }, 1000);
       });
-    }; // end of user.signOut
-    return user;
-  }; // end of cope.user
+    }; // end of `signOut` of object `userAPI`
+    
+    function useSocketIO(socket) {
+      let checkData = socket && socket.handshake 
+                      && socket.handshake.session
+                      && socket.handshake.session.userData;
+      if (checkData) {
+        update(checkData);
+      }
+    }; // end of `useSocketIO` of object `userAPI`
+    
+    return userAPI;
+  }; // end of `user` of object `cope`
 
   // cope.graph
-  cope.graph = function(graphId) {
+  function graph(graphId) {
     let graph = {};
     graph.node = node;
 
@@ -101,20 +116,50 @@ module.exports = function() {
     }; // end of node
 
     return graph;
-  }; // end of cope.graph
+  }; // end of `graph` of object `cope`
 
-  // s stands for socket
-  cope.config = function(s) {
+  // Beware that `useSocketIO` will be called on every single
+  // connection (once a brower is linked to the server, see details in "bin/www").
+  //
+  // Here we set up the connection using socket.io where we take in `s` aka socket in
+  // the file "bin/www".
+  //
+  // So if one day we ditch socket.io, this method should be fully rewitten!
+  function useSocketIO(s) {
     var socket = s, 
         user = cope.user(); // cope user API
     
     if (!socket) { 
-      throw 'Failed to find socket'
+      debug('ERROR', 'Failed to find socket');
+      return;
     }
 
+    user.useSocketIO(s);
+    //TBD: graph.useSocketIO(s);
+
+
+    // The format of the sent objects of "toServer" and "toClient":
+    // "toServer" - {
+    //   api: String,
+    //   data: Mixed,
+    //   reqId: String (optional)
+    // } <- validated by `readReq`
+    //
+    // "toClient" - {
+    //   signal: String,
+    //   data: Mixed,
+    //   reqId: String (optional)
+    // } <- validated by `resObj`
+
     // TBD: make sure this is a private end-to-server socket
+    //
+    // Here we interpret client-side object, and
+    // use server-side Cope api to do the corresponding tasks
     socket.on('toServer', (obj, fn) => {
       try {
+        obj = readReq(obj);
+        if (!obj) { return; }
+
         var api = obj.api,
             data = obj.data,
             reqId = obj.reqId;
@@ -124,6 +169,11 @@ module.exports = function() {
         // translate restful api by using cope.user
         switch (api) {
           case 'u/fetch': 
+            let userData = user.fetch();
+            sendObj(userData ? 'signedIn' : 'signedOut', 
+                    userData,
+                    reqId);
+            /*
             getUser(user => {
               let userData = user.fetch();
               debug('u/fetch:userData', userData);
@@ -133,6 +183,7 @@ module.exports = function() {
                 data: userData
               });
             });
+            */
             break;
           case 'u/signin':
             user.signIn(data).then(function(userData) {
@@ -142,12 +193,7 @@ module.exports = function() {
               socket.handshake.session.save();
 
               debug('u/signin', userData);
-              socket.emit('toClient', {
-                //msg: 'u/signedin_' + data.rid,
-                signal: 'signedIn',
-                reqId: reqId,
-                data: userData
-              });  
+              sendObj('signedIn', userData, reqId);
             }).catch(function(err) {
               // TBD
             });
@@ -159,11 +205,7 @@ module.exports = function() {
               socket.handshake.session.userData = null;
               socket.handshake.session.save();
 
-              socket.emit('toClient', {
-                signal: 'signedOut',
-                reqId: reqId,
-                data: null
-              });  
+              sendObj('signedOut', null, reqId);
             });
             break;
           case 'g/node/set': 
@@ -180,6 +222,12 @@ module.exports = function() {
       } // end of try - catch
     }); // end of socket.on('toServer')
     
+    // The reason for this function, instead of 
+    // directly calling `user.fetch`, is to ensure 
+    // and update whether the user's client session 
+    // reamins signed-in status and to keep the user 
+    // status consistent on both sides
+    /*
     function getUser(callback) {
       let userData = user.fetch() 
         || (socket && socket.handshake 
@@ -193,12 +241,45 @@ module.exports = function() {
         // TBD: error msg
       }
     }; // end of getUser
-    return;
-  }; // end of cope.config
+    */
 
-  // Utilities
+    // Private functions:
+    // Interpret the request object
+    function readReq(obj) {
+      let vo = {};
+      vo.api = obj && obj.api;
+      vo.reqId = obj && obj.reqId;
+      vo.data = obj && obj.data;
+      if (typeof api != 'string') {
+        return null;
+      }
+      return vo;
+    }; // end of `readReq` of function `useSocketIO`
+
+    // To validate the respond object before sending it
+    function sendObj(signal, data, reqId) {
+      if (typeof signal != 'string') {
+        debug('ERROR', 'reqObj: failed to validate respond object in "toClient"');
+        return;
+      }
+      socket.emit('toClient', {
+        signal: signal,
+        data: data, null,
+        reqId: typeof reqId == 'string' ? reqId : null
+      });
+      return;
+    }; // end of `validateRes` of function `useSocketIO`
+
+    return;
+  }; // end of `useSocketIO` of object `cope`
+
+  function useMongoDb() {
+    // TBD
+  }; // end of `useMongoDb` of object `cope`
+
+  // Utilities: private functions of object `cope`
   function makeQueue() {
-    let queue = {},
+    let queue = {};
         
     queue.add = function() {
       
